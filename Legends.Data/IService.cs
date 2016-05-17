0c3c26e54bda0e2134be01156179ea1e2c684d24
Tatiana Veratti <tatianaveratti@gmail.com>
2016-05-17 01:23:30 -0500

    Prepare data/services

---
+++ Legends.Data/IService.cs
@@ -0,0 +1,61 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Legends.Data
+{
+    public interface IService<Entity>
+        where Entity : class
+    {
+        Entity Create(Entity Model);
+        IEnumerable<Entity> Read();
+        Entity Read(long Id);
+        Entity Update(Entity Model);
+        void Delete(long Id);
+    }
+
+    public class Service<Entity> :IService<Entity>
+        where Entity : class
+    {
+        private IDbConnection _cnx;
+        private string _name;
+
+        public Service(string connectionString)
+        {
+            _cnx = new SqlConnection(connectionString);
+            _name = typeof(Entity).Name;
+        }
+
+        public Entity Create(Entity Model)
+        {
+            var spr_name = $"[{_name}_Create]";
+            //return _cnx.Query<Entity>(spr_name, Model, commandType: CommandType.StoredProcedure).FirstOrDefault();
+            return Model;
+        }
+
+        public void Delete(long Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Entity> Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Entity Read(long Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Entity Update(Entity Model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Legends.Data/Services/OrderService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Legends.Models;
+
+namespace Legends.Data.Services
+{
+    public class OrderService
+    {
+        private IService<Order> _orderService;
+
+        public OrderService()
+        {
+            _orderService = new Service<Order>("ConnectionString");
+        }
+    }
+}
